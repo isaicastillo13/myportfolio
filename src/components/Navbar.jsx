9691de2cdc85973b38a72735141b2355d4ea9eb8
Isaias Castillo <isaiascastillo@gmail.com>
2025-01-30 17:22:34 -0500

    Implement profile overlay in Navbar; add click outside functionality and update styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef} from "react";
+import OverlayPorfile from "./Overlays";
 
 const sections = ["Home", "About", "Projects"];
 const hover =
   "hover:text-butterYellow hover:font-semibold transition-all transition-discrete duration-700 ease-in-out";
+//estado para mostrar el overlay
+  
 
 function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+  const overlayRef = useRef(null); 
+
+  useEffect(() => {
+    
+    function handleClickOutside(event) {
+      if (overlayRef.current && !overlayRef.current.contains(event.target)) {
+        setIsVisible(false);
+      }
+    }
+
+    if (isVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isVisible]);
+
   return (
     <nav className="flex justify-between items-center h-auto p-8 font-open-sans text-white text-pearl bg-negro font-beiruti bg-cover bg-center">
-      <div className="flex flex-col gap-2">
-        <img
-          src="./assets/images/fotoPerfil1.png"
-          alt="Profile"
-          className={`${hover} h-16 w-16 object-cover rounded-full border-4 border-white border-solid hover:cursor-pointer hover:border-azulBrillante`}
-        />
-        <div className="flex flex-row items-center justify-center gap-1">
-          <p className="text-[12px]">Yo</p>
-          <img className="w-4 h-4" src="./assets/svg/confirm-circle.svg" alt="" />
+      <div className="flex gap-5">
+        <div className="flex flex-col gap-2">
+          <button onClick={()=>setIsVisible(!isVisible)}>
+          <img
+            src="./assets/images/fotoPerfil1.png"
+            alt="Profile"
+            className={`${hover} h-16 w-16 object-cover rounded-full border-4 border-white border-solid hover:cursor-pointer hover:border-azulBrillante`}
+          />
+          </button>
+          
+          <div className="flex flex-row items-center justify-center gap-1">
+            <p className="text-[12px]">Yo</p>
+            <img
+              className="w-4 h-4"
+              src="./assets/svg/confirm-circle.svg"
+              alt=""
+            />
+          </div>
         </div>
+        {isVisible && (
+          <div ref={overlayRef}>
+            <OverlayPorfile />
+          </div>
+        )}
       </div>
-      
 
       <ul className="flex justify-center space-x-4">
         {sections.map((section, index) => (
--- src/components/overlays.jsx
+++ src/components/overlays.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Overlays() {
+function OverlayPorfile() {
   return (
-    <div className="fixed inset-0 bg-negro bg-opacity-50 z-50 flex justify-center items-center"></div>
+      <div className="flex flex-col gap-2 absolute left-28">
+        <h2 className="font-bakbakOne text-pearl uppercase text-2xl">
+          Isaias E. <br />
+          Castillo
+        </h2>
+        <p className="font-open-sans text-pearl text-[12px] uppercase">
+          html | css | js | Node.js <br />
+          react | Tailwind | sql| figma
+        </p>
+        <Link
+          className="font-bakbakOne text-pearl font-bold text-[14px] uppercase px-[12px] py-1 border-solid border-butterYellow border-4 rounded-full w-fit"
+          to="/#"
+        >
+          ver cv
+        </Link>
+      </div>
   );
 }
+
+export default OverlayPorfile;
