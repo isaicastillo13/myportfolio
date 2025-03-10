0e415e69a346767b151fe8dc6509aff07b4ad442
Isaias Castillo <isaiascastillo@gmail.com>
2025-03-10 15:20:07 -0500

    Refactor Hero and Navbar components; update styles and responsive design in output.css

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,19 +8,19 @@ const hoverClasses =
 
 const Hero = () => {
     return (
-        <div className='h-screen flex flex-col  items-center font-bakbakOne' style={{ backgroundImage: `url(../assets/images/hero-bg.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className='min-h-[900] h-screen flex flex-col  items-center font-bakbakOne' style={{ backgroundImage: `url(../assets/images/hero-bg.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <Navbar />
-            <div className="flex flex-col justify-center items-center gap-6 text-center text-pearl z-10 mt-36">
-                <h1 className="uppercase font-bold text-8xl">Isaias E. Castillo</h1>
-                <p className="font-open-sans text-neutral900 text-2xl max-w-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde nesciunt nostrum vitae atque dolorum.</p>
+            <div className="flex flex-col justify-center items-center gap-4 md:gap-6 text-center text-pearl mt-44 md:mt-36">
+                <h1 className="uppercase font-bold text-4xl md:text-8xl">Isaias E. Castillo</h1>
+                <p className="font-open-sans text-neutral900 text-sm text-center px-4 md:text-2xl md:max-w-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde nesciunt nostrum vitae atque dolorum.</p>
                 <Link
-                    className="group relative flex items-center gap-2 text-neutral900 text-xl uppercase border-4 border-butterYellow py-2 px-4 rounded-full transition duration-300 hover:text-pearl"
+                    className="group  flex items-center gap-2 text-pearl text-sm md:text-xl uppercase border-4 border-butterYellow py-2 px-4 rounded-full transition duration-300 md:hover:text-pearl"
                     to="/portfolio"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     Ver Trabajos
-                    <span className="hidden group-hover:block transition-all transition-discrete duration-700 ease-in-out">
+                    <span className="md:hidden group-hover:block transition-all transition-discrete duration-700 ease-in-out">
                         🚀
                     </span>
                 </Link>
