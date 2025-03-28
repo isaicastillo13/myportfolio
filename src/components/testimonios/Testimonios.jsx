6fd8438986de2274c26545060f5ea418dc4991a2
Isaias Castillo <isaias.castillo@utp.ac.pa>
2025-03-28 11:28:17 -0500

    Add Testimonios component with styling and layout adjustments; update output.css for new utility classes

--- src/components/testimonios/Testimonios.jsx
+++ src/components/testimonios/Testimonios.jsx
@@ -1,7 +1,24 @@
 import React from "react";
  const Testimonios = () => {
     return (
-        <div></div>
+        
+
+<div class="grid my-8 h-screen md:mb-12 md:grid-cols-2">
+    <figure class="flex flex-col items-center justify-center p-8 text-center">
+        <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
+            <p class="my-4">If you care for your time, I hands down would go with this."</p>
+        </blockquote>
+        <figcaption class="flex items-center justify-center ">
+            <img class="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture" />
+            <div class="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
+                <div>Bonnie Green</div>
+                <div class="text-sm text-gray-500 dark:text-gray-400 ">Developer at Open AI</div>
+            </div>
+        </figcaption>    
+    </figure>
+</div>
+
     );
  };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import Hero from './components/hero/Hero';
 import Projects from './components/projects/Projects';
 import Workflow from './components/workflow/Workflow';
+import Testimonios from './components/testimonios/Testimonios';
 import reportWebVitals from './reportWebVitals';
 import '../src/css/output.css';
 
@@ -15,6 +16,7 @@ root.render(
       <Hero />
       <Projects />
       <Workflow />
+      <Testimonios />
     </BrowserRouter>
   </React.StrictMode>
 );
