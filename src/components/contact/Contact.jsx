f424dc4e67483e1341909555a2418c51a3b42b14
Isaias Castillo <isaias.castillo@utp.ac.pa>
2025-04-10 13:11:47 -0500

    Add Testimonios API and component; implement data fetching and error handling

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Contact() {
+    return (
+        // contenedor principal
+        <div>
+            {/* imagen */}
+            <div></div>
+            {/* contenedor de formulario */}
+            <div>
+                {/* info texto */}
+                <div></div>
+                {/* formulario */}
+                <div></div>
+            </div>
+        </div>
+    );
+}
